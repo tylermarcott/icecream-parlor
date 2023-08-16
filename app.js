12049aa247b6cf5270b763b8d7c22fd036d573fd
Tyler Marcott <tylerjmarcott@gmail.com>
2023-08-16 14:19:31 -0600

    Wireframe complete

---
+++ app.js
@@ -0,0 +1,49 @@
+// icecream array with different icecream objects inside
+const iceCream = [{
+  name: 'Cookie Dough',
+  price: 1.25,
+  quantity: 0
+},
+{
+  name: 'Vanilla',
+  price: 1,
+  quantity: 0
+},
+{
+  name: 'Strawberry',
+  price: 1.25,
+  quantity: 0
+}]
+
+//topping array with different topping objects inside
+const toppings = [{
+  name: 'Sprinkles',
+  quantity: 0,
+  price: .25
+},
+{
+  name: 'Chocolate Chips',
+  price: .25,
+  quantity: 0
+},
+{
+  name: 'Gummy Worms',
+  price: .5,
+  quantity: 0
+}]
+
+const vessels = [{
+  name: 'Waffle Cone',
+  price: 1,
+  quantity: 0
+},
+{
+  name: 'Waffle Bowl',
+  price: 1.5,
+  quantity: 0
+},
+{
+  name: '2',
+  price: 2,
+  quantity: 0
+}]
